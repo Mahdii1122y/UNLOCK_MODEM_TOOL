fe0d4004e39119109337d5e9971f0f7b1d969fa2
Mahdi sharifi <101212552+MR-SHARIFI-Dev@users.noreply.github.com>
2026-02-15 09:33:32 +0330

    Update script.js

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,10 +1,10 @@
-// Unlock animation
+// Neon "Unlock" animation
 document.addEventListener('DOMContentLoaded', () => {
-    const logo = document.querySelector('.logo-container h1');
+    const logoText = document.querySelector('.unlock-text');
     let i=0;
     const text = "🔓 Universal Modem Unlocker";
     setInterval(()=>{
-        logo.textContent = text.substring(0,i+1);
+        logoText.textContent = text.substring(0,i+1);
         i = (i+1)%text.length;
     },80);
 });
